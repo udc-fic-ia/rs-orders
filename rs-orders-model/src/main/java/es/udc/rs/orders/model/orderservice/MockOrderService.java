80ee7ce70b785b4ab7b8c1f2f6099e65c0f671db
Manuel Álvarez Díaz <malvarezdiaz@yahoo.es>
2022-08-03 12:29:27 +0200

    Eliminado modificador static de campos / métodos de clase Mock

--- rs-orders-model/src/main/java/es/udc/rs/orders/model/orderservice/MockOrderService.java
+++ rs-orders-model/src/main/java/es/udc/rs/orders/model/orderservice/MockOrderService.java
@@ -9,19 +9,19 @@ import es.udc.rs.orders.model.order.Order;
 
 public class MockOrderService implements OrderService {
 
-	private static Map<Long,Customer> customersMap = new LinkedHashMap<Long,Customer>();
-	private static Map<Long,Order> ordersMap = new LinkedHashMap<Long,Order>();
-	private static Map<Long,List<Order>> ordersByCustomerMap = new LinkedHashMap<Long,List<Order>>();
+	private Map<Long,Customer> customersMap = new LinkedHashMap<Long,Customer>();
+	private Map<Long,Order> ordersMap = new LinkedHashMap<Long,Order>();
+	private Map<Long,List<Order>> ordersByCustomerMap = new LinkedHashMap<Long,List<Order>>();
 
-	private static long lastCustomerId = 0;
-	private static long lastOrderId = 0;
+	private long lastCustomerId = 0;
+	private long lastOrderId = 0;
 	
 
-	private static synchronized long getNextCustomerId() {
+	private synchronized long getNextCustomerId() {
 		return ++lastCustomerId;
 	}
 	
-	private static synchronized long getNextOrderId() {
+	private synchronized long getNextOrderId() {
 		return ++lastOrderId;
 	}
 	
